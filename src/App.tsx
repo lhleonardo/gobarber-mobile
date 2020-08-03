354092f779de01cc9b1f6ce597b2c08c74d571eb
Leonardo Braz <lhleonardo05@gmail.com>
2020-08-03 14:50:27 -0300

    Rotas padrões e páginas iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import 'react-native-gesture-handler';
 
-import { View } from 'react-native';
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
+      <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </View>
+    </>
+  );
 };
 
 export { App };
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Text>Braz</Text>
+    </Container>
+  );
+};
+
+export { SignUp };
