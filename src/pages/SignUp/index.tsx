8abfec42a765ea3b58af838dff2ec9a61db87f96
Leonardo Braz <lhleonardo05@gmail.com>
2020-08-08 01:49:10 -0400

    Cadastro de usuários

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -16,6 +16,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api';
 import extractValidationMessage from '../../utils/extractValidationMessage';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -45,32 +46,38 @@ const SignUp: React.FC = () => {
     );
   }, []);
 
-  const onSubmitForm = useCallback(async (data: SignUpFormData) => {
-    try {
-      formRef.current?.setErrors({});
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .email('E-mail inválido')
-          .required('E-mail obrigatório'),
-        password: Yup.string().required(
-          'Senha obrigatória com grande mensagem de erro',
-        ),
-      });
+  const onSubmitForm = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('E-mail inválido')
+            .required('E-mail obrigatório'),
+          password: Yup.string().required(
+            'Senha obrigatória com grande mensagem de erro',
+          ),
+        });
 
-      await schema.validate(data, { abortEarly: false });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = extractValidationMessage(err);
-        formRef.current?.setErrors(errors);
-      } else {
+        await schema.validate(data, { abortEarly: false });
+        await api.post('/users', data);
+
+        navigation.goBack();
         Alert.alert(
-          'Erro na autenticação',
-          'Usuário e/ou senha incorretos',
-          [],
+          'Tudo pronto!',
+          'Seu usuário foi criado e você já pode entrar na aplicação.',
         );
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = extractValidationMessage(err);
+          formRef.current?.setErrors(errors);
+        } else {
+          Alert.alert('Erro no cadastro', 'Algo de errado aconteceu');
+        }
       }
-    }
-  }, []);
+    },
+    [navigation],
+  );
   return (
     <>
       <KeyboardAvoidingView
@@ -106,7 +113,7 @@ const SignUp: React.FC = () => {
                 keyboardType="email-address"
                 autoCorrect={false}
                 autoCapitalize="none"
-                name="email"
+                name="name"
                 icon="mail"
                 placeholder="E-mail"
                 returnKeyType="next"
@@ -124,7 +131,7 @@ const SignUp: React.FC = () => {
               />
 
               <Button onPress={() => formRef.current?.submitForm()}>
-                Entrar
+                Cadastrar
               </Button>
             </Form>
           </Container>
